b446f45fc9a3d1f7f7cdbf1444e423fe41c07c69
jeffers-surg <janderson@vicarioussurgical.com>
2024-04-29 15:26:57 -0400

    Cleaned up script

--- bin_to_gray.py
+++ bin_to_gray.py
@@ -3,13 +3,6 @@ import argparse
 from PIL import Image
 import cv2
 
-def debayer_image(raw_image_path):
-    # Load the raw image
-
-
-
-    return color_image
-
 def bin_to_png(bin_file, width, height, output_png):
     # Read binary data from file
     with open(bin_file, 'rb') as f:
@@ -40,19 +33,11 @@ def bin_to_png(bin_file, width, height, output_png):
             #Save the pixel data back to a new image array
             reduced_image_data[y_index, x_index] = pixel_data
 
-    # Write out the data
-    # with open("out.cv2", 'wb') as f:
-    #     f.write(reduced_image_data)
-
-    # f.close()
-
-    
-    # debayered_image = debayer_image("out.cv2")
-
     # Apply debayering to convert the raw image to a color image
     debayered_image = cv2.cvtColor(reduced_image_data,  cv2.COLOR_BayerRG2BGR)  # Adjust Bayer pattern based on your camera
 
-    debayered_image = cv2.cvtColor(debayered_image,  cv2.COLOR_BGR2RGB)  # Adjust Bayer pattern based on your camera
+    # Convert to RGB
+    debayered_image = cv2.cvtColor(debayered_image,  cv2.COLOR_BGR2RGB)
 
     # # Create PIL image
     image = Image.fromarray(debayered_image, mode='RGB')  # 'L' mode for grayscale
@@ -63,15 +48,6 @@ def bin_to_png(bin_file, width, height, output_png):
     image.save(output_png)
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='Convert 8-bit binary per pixel bin file to grayscale PNG')
-    # parser.add_argument('bin_file', type=str, help='Input binary file')
-    # parser.add_argument('width', type=int, help='Width of the image')
-    # parser.add_argument('height', type=int, help='Height of the image')
-    # parser.add_argument('output_png', type=str, help='Output PNG file')
-    # args = parser.parse_args()
-
-    #bin_to_png(args.bin_file, args.width, args.height, args.output_png)
-
     bin_file = "out.ppm"
     width = 4096
     height = 1540
