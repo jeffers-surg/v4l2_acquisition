555dfa55e0539f9a1315abcf444377544cca94d2
jeffers-surg <janderson@vicarioussurgical.com>
2024-05-14 12:22:20 -0400

    Update 5

--- capture.cpp
+++ capture.cpp
@@ -46,7 +46,6 @@
 #include <linux/videodev2.h>
 
 #include <cuda_runtime.h>
-#include "yuv2rgb.cuh"
 
 #include <iostream>
 
@@ -243,25 +242,6 @@ uninit_device                   (void)
 }
 
 static void
-init_read                       (unsigned int           buffer_size)
-{
-    buffers = (struct buffer *) calloc (1, sizeof (*buffers));
-
-    if (!buffers) {
-        fprintf (stderr, "Out of memory\n");
-        exit (EXIT_FAILURE);
-    }
-
-    buffers[0].length = buffer_size;
-    buffers[0].start = malloc (buffer_size);
-
-    if (!buffers[0].start) {
-        fprintf (stderr, "Out of memory\n");
-        exit (EXIT_FAILURE);
-    }
-}
-
-static void
 init_mmap                       (void)
 {
     struct v4l2_requestbuffers req;
@@ -317,54 +297,6 @@ init_mmap                       (void)
 
         if (MAP_FAILED == buffers[n_buffers].start)
             errno_exit ("mmap");
-    }
-}
-
-static void
-init_userp                      (unsigned int           buffer_size)
-{
-    struct v4l2_requestbuffers req;
-    unsigned int page_size;
-
-    page_size = getpagesize ();
-    buffer_size = (buffer_size + page_size - 1) & ~(page_size - 1);
-
-    CLEAR (req);
-
-    req.count               = 1;
-    req.type                = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory              = V4L2_MEMORY_USERPTR;
-
-    if (-1 == xioctl (fd, VIDIOC_REQBUFS, &req)) {
-        if (EINVAL == errno) {
-            fprintf (stderr, "%s does not support "
-                    "user pointer i/o\n", dev_name);
-            exit (EXIT_FAILURE);
-        } else {
-            errno_exit ("VIDIOC_REQBUFS");
-        }
-    }
-
-    buffers = (struct buffer *) calloc (4, sizeof (*buffers));
-
-    if (!buffers) {
-        fprintf (stderr, "Out of memory\n");
-        exit (EXIT_FAILURE);
-    }
-
-    for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
-        buffers[n_buffers].length = buffer_size;
-        if (cuda_zero_copy) {
-            cudaMallocManaged (&buffers[n_buffers].start, buffer_size, cudaMemAttachGlobal);
-        } else {
-            buffers[n_buffers].start = memalign (/* boundary */ page_size,
-                    buffer_size);
-        }
-
-        if (!buffers[n_buffers].start) {
-            fprintf (stderr, "Out of memory\n");
-            exit (EXIT_FAILURE);
-        }
     }
 }
 
@@ -483,160 +415,6 @@ openCamera                     (void)
     }
 
     return true;
-}
-
-static void
-init_cuda                       (void)
-{
-    /* Check unified memory support. */
-    if (cuda_zero_copy) {
-        cudaDeviceProp devProp;
-        cudaGetDeviceProperties (&devProp, 0);
-        if (!devProp.managedMemory) {
-            printf ("CUDA device does not support managed memory.\n");
-            cuda_zero_copy = false;
-        }
-    }
-
-    /* Allocate output buffer. */
-    size_t size = width * height * 3;
-    if (cuda_zero_copy) {
-        cudaMallocManaged (&cuda_out_buffer, size, cudaMemAttachGlobal);
-    } else {
-        cuda_out_buffer = (unsigned char *) malloc (size);
-    }
-
-    cudaDeviceSynchronize ();
-}
-
-static void
-usage                           (FILE *                 fp,
-                                 int                    argc,
-                                 char **                argv)
-{
-    fprintf (fp,
-            "Usage: %s [options]\n\n"
-            "Options:\n"
-            "-c | --count N       Frame count (default: %u)\n"
-            "-d | --device name   Video device name (default: %s)\n"
-            "-f | --format        Capture input pixel format (default: UYVY)\n"
-            "-h | --help          Print this message\n"
-            "-m | --mmap          Use memory mapped buffers\n"
-            "-o | --output        Output file name (default: %s)\n"
-            "-s | --size WxH      Frame size (default: %ux%u)\n"
-            "-u | --userp         Use application allocated buffers\n"
-            "-z | --zcopy         Use zero copy CUDA memory\n"
-            "Experimental options:\n"
-            "-r | --read          Use read() calls\n"
-            "-F | --field         Capture field (default: INTERLACED)\n"
-            "",
-            argv[0], count, dev_name, file_name, width, height);
-}
-
-static const char short_options [] = "c:d:f:F:hmo:rs:uz";
-
-static const struct option
-long_options [] = {
-    { "count",      required_argument,      NULL,           'c' },
-    { "device",     required_argument,      NULL,           'd' },
-    { "format",     required_argument,      NULL,           'f' },
-    { "field",      required_argument,      NULL,           'F' },
-    { "help",       no_argument,            NULL,           'h' },
-    { "mmap",       no_argument,            NULL,           'm' },
-    { "output",     required_argument,      NULL,           'o' },
-    { "read",       no_argument,            NULL,           'r' },
-    { "size",       required_argument,      NULL,           's' },
-    { "userp",      no_argument,            NULL,           'u' },
-    { "zcopy",      no_argument,            NULL,           'z' },
-    { 0, 0, 0, 0 }
-};
-
-static struct {
-    const char *name;
-    unsigned int fourcc;
-} pixel_formats[] = {
-    { "RGB332", V4L2_PIX_FMT_RGB332 },
-    { "RGB555", V4L2_PIX_FMT_RGB555 },
-    { "RGB565", V4L2_PIX_FMT_RGB565 },
-    { "RGB555X", V4L2_PIX_FMT_RGB555X },
-    { "RGB565X", V4L2_PIX_FMT_RGB565X },
-    { "BGR24", V4L2_PIX_FMT_BGR24 },
-    { "RGB24", V4L2_PIX_FMT_RGB24 },
-    { "BGR32", V4L2_PIX_FMT_BGR32 },
-    { "RGB32", V4L2_PIX_FMT_RGB32 },
-    { "Y8", V4L2_PIX_FMT_GREY },
-    { "Y10", V4L2_PIX_FMT_Y10 },
-    { "Y12", V4L2_PIX_FMT_Y12 },
-    { "Y16", V4L2_PIX_FMT_Y16 },
-    { "UYVY", V4L2_PIX_FMT_UYVY },
-    { "VYUY", V4L2_PIX_FMT_VYUY },
-    { "YUYV", V4L2_PIX_FMT_YUYV },
-    { "YVYU", V4L2_PIX_FMT_YVYU },
-    { "NV12", V4L2_PIX_FMT_NV12 },
-    { "NV21", V4L2_PIX_FMT_NV21 },
-    { "NV16", V4L2_PIX_FMT_NV16 },
-    { "NV61", V4L2_PIX_FMT_NV61 },
-    { "NV24", V4L2_PIX_FMT_NV24 },
-    { "NV42", V4L2_PIX_FMT_NV42 },
-    { "SBGGR8", V4L2_PIX_FMT_SBGGR8 },
-    { "SGBRG8", V4L2_PIX_FMT_SGBRG8 },
-    { "SGRBG8", V4L2_PIX_FMT_SGRBG8 },
-    { "SRGGB8", V4L2_PIX_FMT_SRGGB8 },
-    { "SBGGR10_DPCM8", V4L2_PIX_FMT_SBGGR10DPCM8 },
-    { "SGBRG10_DPCM8", V4L2_PIX_FMT_SGBRG10DPCM8 },
-    { "SGRBG10_DPCM8", V4L2_PIX_FMT_SGRBG10DPCM8 },
-    { "SRGGB10_DPCM8", V4L2_PIX_FMT_SRGGB10DPCM8 },
-    { "SBGGR10", V4L2_PIX_FMT_SBGGR10 },
-    { "SGBRG10", V4L2_PIX_FMT_SGBRG10 },
-    { "SGRBG10", V4L2_PIX_FMT_SGRBG10 },
-    { "SRGGB10", V4L2_PIX_FMT_SRGGB10 },
-    { "SBGGR12", V4L2_PIX_FMT_SBGGR12 },
-    { "SGBRG12", V4L2_PIX_FMT_SGBRG12 },
-    { "SGRBG12", V4L2_PIX_FMT_SGRBG12 },
-    { "SRGGB12", V4L2_PIX_FMT_SRGGB12 },
-    { "DV", V4L2_PIX_FMT_DV },
-    { "MJPEG", V4L2_PIX_FMT_MJPEG },
-    { "MPEG", V4L2_PIX_FMT_MPEG },
-};
-
-static unsigned int v4l2_format_code(const char *name)
-{
-    unsigned int i;
-
-    for (i = 0; i < ARRAY_SIZE(pixel_formats); ++i) {
-        if (strcasecmp(pixel_formats[i].name, name) == 0)
-            return pixel_formats[i].fourcc;
-    }
-
-    return 0;
-}
-
-static struct {
-    const char *name;
-    unsigned int field;
-} fields[] = {
-    { "ANY", V4L2_FIELD_ANY },
-    { "NONE", V4L2_FIELD_NONE },
-    { "TOP", V4L2_FIELD_TOP },
-    { "BOTTOM", V4L2_FIELD_BOTTOM },
-    { "INTERLACED", V4L2_FIELD_INTERLACED },
-    { "SEQ_TB", V4L2_FIELD_SEQ_TB },
-    { "SEQ_BT", V4L2_FIELD_SEQ_BT },
-    { "ALTERNATE", V4L2_FIELD_ALTERNATE },
-    { "INTERLACED_TB", V4L2_FIELD_INTERLACED_TB },
-    { "INTERLACED_BT", V4L2_FIELD_INTERLACED_BT },
-};
-
-static unsigned int v4l2_field_code(const char *name)
-{
-    unsigned int i;
-
-    for (i = 0; i < ARRAY_SIZE(fields); ++i) {
-        if (strcasecmp(fields[i].name, name) == 0)
-            return fields[i].field;
-    }
-
-    return -1;
 }
 
 int main()
